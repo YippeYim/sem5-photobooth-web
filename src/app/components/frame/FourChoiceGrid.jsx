ec90d337f7bf67bfc40830d28408b24f2f380b87
YippeYim <piya.yim2003@gmail.com>
2026-03-28 13:25:37 +0700

    responsive font and grid size

--- src/app/components/frame/FourChoiceGrid.jsx
+++ src/app/components/frame/FourChoiceGrid.jsx
@@ -1,7 +1,7 @@
 export function FourChoiceGrid({children}){
     return <>
-        <div className="flex justify-center h-screen">
-            <div className="grid grid-cols-2 gap-14 w-2/3 h-2/3 mt-15">
+        <div className="flex justify-center h-screen max-h-(30vh)">
+            <div className="grid grid-cols-2 max-h-[60vw] gap-14 w-2/3 h-2/3 mt-15">
                 {children}
             </div>
         </div>
