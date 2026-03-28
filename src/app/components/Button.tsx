07d7ca7be5e98dbc7493d81d3ecb5ec7dede4001
YippeYim <piya.yim2003@gmail.com>
2026-03-28 16:01:01 +0700

    fix build deploy error

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,29 @@
+/**
+ * A reusable button component with customizable styles.
+ */
+
+// 1. Define the Interface
+interface ButtonProps {
+  buttonType?: "primary" | "secondary";
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+// 2. Apply the Interface to the function
+export function Button({ buttonType, onClick, children }: ButtonProps) {
+  const buttonStyle =
+    buttonType === "primary"
+      ? "bg-button-primary text-white border-button-primary"
+      : "bg-button-secondary text-gray-600 border-white";
+
+  return (
+    <button
+      className={`${buttonStyle} rounded-full py-1 px-4 
+                  border-4 cursor-pointer transition-all duration-300
+                  hover:bg-white hover:text-black hover:border-button-primary`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
